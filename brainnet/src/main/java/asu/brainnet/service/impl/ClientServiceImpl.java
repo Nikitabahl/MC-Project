36859cdd05cc0026f0ab148385f1bf650ecc3190
sahebjohar92 <sahebsingh10@gmail.com>
2018-11-27 16:17:04 -0700

    Adding matlab jars

--- brainnet/src/main/java/asu/brainnet/service/impl/ClientServiceImpl.java
+++ brainnet/src/main/java/asu/brainnet/service/impl/ClientServiceImpl.java
@@ -1,28 +1,32 @@
 package asu.brainnet.service.impl;
 
+import SVM.Class3;
 import asu.brainnet.model.BrainSignalsInfo;
 import asu.brainnet.service.ClientServiceInterface;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
-
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.mathworks.toolbox.javabuilder.MWArray;
+import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWNumericArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class ClientServiceImpl implements ClientServiceInterface {
@@ -30,6 +34,7 @@ public class ClientServiceImpl implements ClientServiceInterface {
     private String awsS3AudioBucket;
     private AmazonS3 amazonS3;
     private MongoTemplate mongoTemplate;
+    private static final String authenticateFileName = "authenticateFile.edf";
     private static final Logger logger = LoggerFactory.getLogger(ClientServiceImpl.class);
 
     public ClientServiceImpl(Region awsRegion, AWSCredentialsProvider awsCredentialsProvider, String awsS3AudioBucket,
@@ -113,13 +118,41 @@ public class ClientServiceImpl implements ClientServiceInterface {
             fos.write(multipartFile.getBytes());
             fos.close();
 
-            //Run Ml algorithm
-            isAuthenticated = true;
+            File authenticationFile = new File(authenticateFileName);
+            FileOutputStream authenticationFileOS = new FileOutputStream(authenticationFile);
+            authenticationFileOS.write(brainSignalsInfo.getFileContent().getBytes());
+            authenticationFileOS.close();
+
+            isAuthenticated = authenticateFiles(fileName, authenticateFileName);
 
         } catch (Exception e) {
             logger.error("Unable to authenticate the user", e);
         }
         return isAuthenticated;
+    }
+
+    private boolean authenticateFiles(String incomingFile, String registeredFile) throws MWException {
+
+        File f = new File(incomingFile);
+
+        if (f.exists()) {
+
+            Object[] resultSum = null;
+            Class3 svm = new Class3();
+
+            resultSum = svm.SVM(1, incomingFile, registeredFile);
+
+            MWArray javaSum = (MWNumericArray) resultSum[0];
+
+            double[][] total = (double[][])javaSum.toArray();
+
+            int finalResult = (int) total[0][0];
+
+            return finalResult == 1;
+        }
+        else {
+            return false;
+        }
     }
 
     private static String readFile(MultipartFile file) throws IOException {
